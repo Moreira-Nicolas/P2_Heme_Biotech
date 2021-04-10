8819934f6bc695ccbe22aff4cdc6db424ba30003
Nicolas Moreira <moreira.n@outlook.fr>
2021-04-10 10:39:03 +0200

    Add AnalyticsCounter MainClass

---
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,20 @@
+package com.hemebiotech.analytics;
+
+import java.util.TreeMap;
+
+/**
+ * Main class
+ **/
+public class AnalyticsCounter {
+	/**
+	 * Main function (not modified)
+	 **/
+	public static void main(String args[]){
+
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		TreeMap<String, Integer> resultSymptoms = reader.getSymptoms();
+
+		WriterCountSymptoms  writer = new WriterCountSymptoms("result.out");
+		writer.symptomsWriter(resultSymptoms);
+		}
+	}
